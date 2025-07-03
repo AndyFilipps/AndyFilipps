81292ae89e6322a26d7b20e84439cb786a39aa44
Andrey Filips <Andy.Filipps@gmail.com>
2025-07-03 01:23:42 +0000

    fact trip created

---
+++ models/fact_trip.sql
@@ -0,0 +1,16 @@
+with bike_cte as
+(
+select 
+    ride_id ,
+    rideable_type ,
+    started_at ,
+    ended_at ,
+    timestampdiff(seconds,started_at, ended_at) as Trip_Duration_sec,
+    member_casual, 
+    START_STATION_ID,
+    END_STATION_ID
+
+from {{ source('test', 'bike') }}
+    
+)
+select * from bike_cte
